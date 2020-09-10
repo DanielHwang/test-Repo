ab97b7e4c05c48987f4eb5bf172b2905768680e2
Daniel <peniel5@gmail.com>
2020-09-10 09:44:27 -0400

    commit

--- hello.py
+++ hello.py
@@ -1,21 +1,21 @@
-# import os
-# import sys
+import os
+import sys
 
 import requests
 
-# print(sys.version)
-# print(sys.executable)
+print(sys.version)
+print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = "hello, {}".format(who_to_greet)
-#     return greeting
+def greet(who_to_greet):
+    greeting = "hello, {}".format(who_to_greet)
+    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-# print(greet("Daniel"))
+print(greet("Daniel"))
 
 
-# name = input("Your name? ")
-# print("hello, ", name)
+name = input("Your name? ")
+print("hello, ", name)
